fa5771623603ef28e80124869763a9d4699907b7
Sander <zpam_zpam@hotmail.com>
2022-07-21 18:24:16 +0200

    Unicafe 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,6 +2,17 @@ import { useState } from 'react'
 
 const Button =  (props) => <button onClick={props.handleClick}>{props.text}</button>
 const ShowText = (props) => <div>{props.text} {props.value} {props.posttext}</div>
+const Statistics = (props) => {
+  return <div>
+    <h1>statistics</h1>
+    <ShowText text="good" value={props.good} />
+    <ShowText text="neutral" value={props.neutral} />
+    <ShowText text="bad" value={props.bad} />
+    <ShowText text="all" value={props.good + props.neutral + props.bad} />
+    <ShowText text="average" value={(props.good-props.bad)/(props.good + props.neutral + props.bad)} />
+    <ShowText text="positive" value={(props.good/(props.good + props.neutral + props.bad)*100)} posttext="%"/>
+  </div>
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -20,14 +31,9 @@ const App = () => {
       <Button text="neutral" handleClick={handleNeutralClick} />
       <Button text="bad" handleClick={handleBadClick} />
 
-      <h1>statistics</h1>
-      <ShowText text="good" value={good} />
-      <ShowText text="neutral" value={neutral} />
-      <ShowText text="bad" value={bad} />
-      <ShowText text="all" value={good + neutral + bad} />
-      <ShowText text="average" value={(good-bad)/(good + neutral + bad)} />
-      <ShowText text="positive" value={(good/(good + neutral + bad)*100)} posttext="%"/>
-    </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+
+   </div>
   )
 }
 
