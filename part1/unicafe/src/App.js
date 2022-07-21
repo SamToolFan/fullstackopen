6d9d88550ec2af1561b2187084d8ee38104c6a96
Sander <zpam_zpam@hotmail.com>
2022-07-21 17:42:42 +0200

    Unicafe 1.7 - Improved - not defining components within components now

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 
+const Button =  (props) => <button onClick={props.handleClick}>{props.text}</button>
+const ShowText = (props) => <div>{props.text} {props.value} {props.posttext}</div>
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const Button =  (props) => <button onClick={props.handleClick}>{props.text}</button>
-  const ShowText = (props) => <div>{props.text} {props.value} {props.posttext}</div>
 
   const handleGoodClick = () => setGood(good + 1)
   const handleNeutralClick = () => setNeutral(neutral + 1)
