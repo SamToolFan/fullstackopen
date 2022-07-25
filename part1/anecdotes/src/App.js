2933947475736d07f234c70874995b72435434bc
Sander <zpam_zpam@hotmail.com>
2022-07-25 12:11:04 +0200

    Anecdotes 1.13 - It works but some alternatives I tried didn't and I do not fully understand why - It works now

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 
-
 const Button = (props) => {
   return(
-    <button onClick={props.onClick}>{props.text}</button>
+    <button onClick={props.handleClick}>{props.text}</button>
   )
 }
 
 const App = () => {
- 
+  const getRandomInt = (max) => Math.floor(Math.random() * max)
   const anecdotes = [
     'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
@@ -18,21 +17,40 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
   ]
-  
-  const getRandomInt = (max) => Math.floor(Math.random() * max)
+  const [selected, setSelected] = useState(getRandomInt(anecdotes.length))  // I don't always want to start with the first anecdote
+  const [votes, setVoted] = useState(Array(anecdotes.length).fill(0))
 
-  const [selected, setSelected] = useState(0)
+//  const randomnumber = getRandomInt(anecdotes.length)
+//  const handleNextanecdote = () => setSelected(randomnumber)
+//  The two lines above make that at a certain moment the buttons become unresponsive
+//  Doing it like below solves the problem - it took me several hours before I found it but I DON'T GET IT!!!!!!!
+  const handleNextanecdote = () => setSelected(getRandomInt(anecdotes.length))
 
-  const setRandomnumber = () => setSelected(getRandomInt(anecdotes.length))
- 
+  const handleVote = () =>   {
+//  newVotes = votes
+//  Defining newVotes like above doesn't work properly. It appears the array is different then defining at as below
+//  Result is that the setVoted used below doesn't rerender the screen but the counts are recorded so later the correct number of votes is shown. I don't fully comprehend this
+    const newVotes = {
+      ...votes,
+    }
+    newVotes[selected] += 1
+    setVoted(newVotes)
+  }
+
   return (
     <div>
-      {anecdotes[selected]}
       <p>
-      <Button onClick={setRandomnumber} text="Random anecdote"/>
+        {anecdotes[selected]}
+      </p>
+      <p>
+        Has {votes[selected]} votes
+      </p>
+      <p>
+      <Button text="Vote" handleClick={handleVote}/>
+      <Button text="Random anecdote" handleClick={handleNextanecdote}/>
       </p>
     </div>
   )
 }
 
-export default App
+export default App 
