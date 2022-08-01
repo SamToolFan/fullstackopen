4de7f76e337cf4fc3863bc3f93834d0d18d76236
Sander <zpam_zpam@hotmail.com>
2022-08-01 20:07:30 +0200

    Phonebook 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,24 @@
 import { useState } from 'react'
 
-const Showpersons = ({persons}) => 
-  <> 
-    {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
-  </>
+const Filter = (persons, filter) => persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
 
+const Showpersons = (props) => 
+    <> 
+      {Filter(props.persons, props.filter).map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+    </>
 
 const App = () => 
 {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ])
+    { name: 'Arto Hellas', number: '040-1234567' },
+    { name: 'Sander van Waard', number: '45678298765' },
+    { name: 'Marije Koekebacker', number: '98765432' },
+    { name: 'Jan Jansen', number: '34567890' }
+   ])
 
   const [newName, setNewName] = useState('Enter a name to add...')
   const [newNumber, setNewNumber] = useState('Enter a telephonenumber...')
+  const [filterName, setFilter] = useState('')
 
   const addName = (event) =>    //This function is executed when the button is pressed (and the new name is submitted)
   {
@@ -34,6 +39,7 @@ const App = () =>
   
         setNewName('')     //reset the default value
         setNewNumber('')     //reset the default value
+        setFilter('')     //reset filter so an added name is always shown
       }
   }
 
@@ -49,9 +55,18 @@ const App = () =>
     setNewNumber(event.target.value)
   }
 
+  const handleFilter = (event) => {
+    //console.log(event.target.value)
+    setFilter(event.target.value)
+}
+
   return (
     <div>
       <h2>Phonebook</h2>
+        <div>
+          filter shown with: <input  value={filterName} onChange={handleFilter}/>
+        </div>
+      <h2>Add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameAdd} />
@@ -64,7 +79,7 @@ const App = () =>
         </div>
       </form>
       <h2>Numbers</h2>
-      <Showpersons persons={persons}/>
+      <Showpersons persons={persons} filter={filterName}/>
     </div>
   )
 }
